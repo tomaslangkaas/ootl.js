ee41713e32aaef613b2d81332e56ebc80d9a8f99
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2015-11-02 14:52:51 +0100

    Create ootl.js

---
+++ ootl.js
@@ -0,0 +1,15 @@
+var ootl = {
+  removeClass: function(elm, cls){
+    var w = '';
+    elm.className = (w + elm.className + w)
+      .split(w + cls + w)
+      .join(w)
+      .slice(1,-1);
+  },
+  addClass: function(elm, cls){
+    elm.className = elm.className + ' ' + cls;
+  },
+  elm: function(id){
+    return document.getElementById(id) || {};
+  }
+};
